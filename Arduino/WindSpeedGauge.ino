973cc01be56b8f98952a7ba61cfc57c39d0e19b5
Andy Hudson-Smith <50172263+digitalurban@users.noreply.github.com>
2023-11-09 13:32:40 +0000

    Update WindSpeedGauge.ino
    Updated for use of Neopixels and changed MQTT Port to open feed

--- Arduino/WindSpeedGauge.ino
+++ Arduino/WindSpeedGauge.ino
@@ -1,16 +1,22 @@
 
 
-#include "ServoEasing.hpp"
-#include <ESP8266WiFi.h>
+#include <ServoEasing.hpp>
 #include <WiFiClient.h>
+#include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include <NTPClient.h>
 #include <WiFiUdp.h>
 
-//note ServoEasing needs to be version 2.4.x
+#include <Adafruit_NeoPixel.h>
 
+
+#define LED_COUNT 47
+#define LED_PIN 5
+
+
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN,  NEO_RGBW + NEO_KHZ800); //edit RGB / RGBW according to Pixel Strip Type
 // Script turns on and off at set times and makes use of 2 LEDs and
-// 180 degree servo, an SG90 is recomended. Example code is for a 270 degree Guage
+// 180 degree servo, an SG90S is recomended. Example code is for Wind Speed
 // using a Wind Speed mqtt feed.
 // It uses the 2:14 gear train set up in the Connected Environments Workshop
 // The code can bbe adaopted for other servo types/timezones and data feeds
@@ -23,70 +29,59 @@ ServoEasing servo;
 // create an instance of the servo class
 
 int angle;
+int pixel;
 
 // set up night mode deep sleep
 
 
 // connect to wifi and mqtt server
 
-const char* ssid = "yourwifi";
-const char* password =  "wifipassword";
-const char* mqttServer = "mqttserver";
-const int mqttPort = mqttport;
-const char* mqttUser = "mqttusername";
-const char* mqttPassword = "mqttpassword";
-WiFiClient espWindSpeedLecture;
-PubSubClient client(espWindSpeedLecture);
+const char* ssid = "YourWifi";
+const char* password =  "WifiPassword";
+const char* mqttServer = "mqtt.cetools.org";  //Edit this for your own MQTT or leave for the CE Wind Speed Feed
+const int mqttPort = 1883;
 
-// Define NTP Client to get time
+WiFiClient espWindSpeedLecture23;
+PubSubClient client(espWindSpeedLecture23);
 
-WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, "pool.ntp.org");
 
 
 void setup()
 
 
 {
-   digitalWrite(2, HIGH);
   
-   // Initialize a NTPClient to get time
-  timeClient.begin();
-  // Set offset time in seconds to adjust for your timezone, for example:
-  // GMT +1 = 3600
-  // GMT +8 = 28800
-  // GMT -1 = -3600
-  // GMT 0 = 0
-  timeClient.setTimeOffset(0);
 
-  // turn on LEDS and Setup Servo Sweep
-  pinMode (LED_BUILTIN, OUTPUT); // Define LED as output
-  pinMode(5, OUTPUT);  //LED STRIP1
-  pinMode(10, OUTPUT);  //LED STRIP2
-  
+  strip.begin();
+ //strip.setPixelColor(1, 0, 0, 0, 255);
+
+ uint32_t white = strip.Color(0, 0, 0, 255);
+ strip.fill(white, 0, 27);  // edit according to pixel count
+
+  strip.show(); 
   WiFi.mode(WIFI_STA);
   WiFi.softAPdisconnect(true);
 
-  // Start up Servo Sweep - also allows calibration
-
-  servo.write(30); 
+  servo.write(3); 
   servo.attach(4);
 
-
-  servo.setEasingType(EASE_SINE_IN_OUT);
+// Servo Sweep - Edit for own Servo/Dial Range
+  servo.setEasingType(EASE_CUBIC_IN_OUT);
   servo.setSpeed(20);
   Serial.print("Moving to 0");
-  servo.easeTo(0);
+  servo.easeTo(10);
+  
   delay (1000);
   Serial.print("Moving to 270");
-  servo.easeTo(127);
+  servo.easeTo(140);
+ 
   delay (1000);
+  
   Serial.print("Moving to 0");
-  servo.easeTo(0);
+  servo.easeTo(10);
   Serial.print("Pausing for 5 seconds");
   delay (5000);
 
-// Keep Wifi from Sleeping
 
   WiFi.mode(WIFI_STA);
   WiFi.softAPdisconnect(true);
@@ -105,7 +100,7 @@ void setup()
   client.setCallback(callback);
   while (!client.connected()) {
     Serial.println("Connecting to MQTT...");
-    if(client.connect("espWindSpeedLecture", mqttUser, mqttPassword)) 
+    if(client.connect("espWindSpeedLecture23")) 
 {
      Serial.println("connected");
      
@@ -116,7 +111,7 @@ void setup()
     }
   }
  
-  client.subscribe("downhamweather/windSpeed_mph");
+  client.subscribe("personal/ucfnaps/downhamweather/windSpeed_mph");
 }
 //Reconnect if connection lost
 void reconnect() {
@@ -132,7 +127,7 @@ void reconnect() {
       // Once connected, publish an announcement...
      
       // ... and resubscribe
-      client.subscribe("downhamweather/windSpeed_mph");
+      client.subscribe("personal/ucfnaps/downhamweather/windSpeed_mph");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -142,8 +137,6 @@ void reconnect() {
     }
   }
 }
-
-
 
 
 
@@ -164,63 +157,28 @@ void callback(const char* topic, byte* payload, unsigned int length) {
   payload[length] = '\0';
   String w = String((char*)payload);
   int wind = w.toInt();
-    
-  angle = map(wind, 0, 60, 0, 127);
+  
+  // Edit the range to match your own servo configeration  
+  angle = map(wind, 0, 40, 10, 140);
 
  
-
- // Move the servo according to the data 
   
-  servo.setEasingType(EASE_SINE_IN_OUT);
-  servo.setSpeed(10);
+  servo.setEasingType(EASE_CUBIC_IN_OUT);
+  servo.setSpeed(15);
 //servo.write(angle);
   servo.easeTo(angle);
   Serial.print("Wind Speed ");
   Serial.println(wind);
-  client.publish("Devices/WindSpeedDial/","Wind Speed Alive");
-  delay(1000);   // Allow transit time
+  delay(500);   // Allow transit time
+  
  
 }
 
 void loop()
 
 {
-    // get time
-
-  timeClient.update();
-
-  unsigned long epochTime = timeClient.getEpochTime();
-  String formattedTime = timeClient.getFormattedTime();
-  Serial.print("Formatted Time: ");
-  Serial.println(formattedTime);  
-  int currentHour = timeClient.getHours();
-  int currentHour24 = timeClient.getHours();
-  int currentMinute = timeClient.getMinutes();
-  
-  //Get a time structure
-  struct tm *ptm = gmtime ((time_t *)&epochTime); 
-
-// Set up the Awake time - this turns the device on between 7am and 10pm
-// and turns off lights, moves servo to 0 if outside this time. 
-  
- if ((currentHour24 >= 7) && (currentHour24 <= 22)) {
-
- // Turns lights on - runs once
  
- digitalWrite(2, HIGH);
- digitalWrite(5, HIGH);
- digitalWrite(10, HIGH);
-
-// connect to mqtt if not already connected
-  if (!client.connected()) {
-    client.connect(" espWindSpeedLecture", mqttUser, mqttPassword);
-// Your MQTT topic     
-    client.subscribe("downhamweather/windSpeed_mph");
-
-    
-  }
-  
- //wait for 1s, regularly calling the mqtt loop function
+  // and now wait for 1s, regularly calling the mqtt loop function
   for (int i=0; i<1000; i++) {
     client.loop();
     delay(1);
@@ -228,16 +186,4 @@ void loop()
   
    }
 
-// If its not within the set hours - turn off light and servo
-  else{
-    Serial.print("Sleeping");
-    servo.easeTo(0);
-    delay(1000);
-    digitalWrite(2, HIGH);
-    digitalWrite(5, LOW);
-    digitalWrite(10, LOW);
-    delay(60000);
-
-  
-  }
-  }
+ 
